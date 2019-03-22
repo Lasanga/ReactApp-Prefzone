7be2b00641d4fc8a87a26be43ed79050978d37fb
Lasanga Guruge <lasanga.guruge@agileinnovationsgroup.com>
2019-03-23 03:19:16 +0530

    Changes in Home Screen

--- app/components/Home/HomeScreen.js
+++ app/components/Home/HomeScreen.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import { DrawerNavigator } from 'react-navigation';
 
 class HomeScreen extends Component {
     render() {
         const { navigation } = this.props;
         const userDetails = navigation.getParam('userDetails', 'NO-ID');
         return (
-            <View>
-                <Text> {userDetails.userId} </Text>
-            </View>
+
+            <AppStack />
+            // <View>
+            //     <Text> {userDetails.userId} </Text>
+            // </View>
         )
     }
 }
+
+const AppStack = DrawerNavigator({
+    Home: HomeScreen
+})
 
 export default HomeScreen;
